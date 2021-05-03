df9c0be5df2226cbec7f854d409015f92ac7152f
Nadheer Chatharoo <>
2021-05-03 18:45:41 +0200

    Create app with a Hello world

---
+++ SwiftUIMovies/SwiftUIMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMoviesApp.swift
+//  SwiftUIMovies
+//
+//  Created by Nadheer on 03/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUIMoviesMac/SwiftUIMoviesMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMoviesMacApp.swift
+//  SwiftUIMoviesMac
+//
+//  Created by Nadheer on 03/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMoviesMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
