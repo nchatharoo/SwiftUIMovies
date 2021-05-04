4ef4b5ddefcb782d465466f085518ee16515a933
Nadheer Chatharoo <>
2021-05-04 13:05:12 +0200

    Replace optional params with enum with success/failure

--- SwiftUIMoviesTests/LoadMovieFromRemoteUseCaseTests.swift
+++ SwiftUIMoviesTests/LoadMovieFromRemoteUseCaseTests.swift
@@ -9,18 +9,18 @@ import XCTest
 import SwiftUIMovies
 
 class HTTPClientSpy: HTTPClient {
-    private var messages = [(url: URL, completion: (Error?, HTTPURLResponse?) -> Void)]()
+    private var messages = [(url: URL, completion: (HTTPClientResult) -> Void)]()
     
     var requestedURLs: [URL] {
         return messages.map { $0.url }
     }
-
-    func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void) {
+    
+    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
         messages.append((url, completion))
     }
     
     func complete(with error: Error, at index: Int = 0) {
-        messages[index].completion(error, nil)
+        messages[index].completion(.failure(error))
     }
     
     func complete(withStatusCode code: Int, at index: Int = 0) {
@@ -29,8 +29,8 @@ class HTTPClientSpy: HTTPClient {
             statusCode: code,
             httpVersion: nil,
             headerFields: nil
-        )
-        messages[index].completion(nil, response)
+        )!
+        messages[index].completion(.success(response))
     }
 }
 
@@ -42,14 +42,16 @@ class LoadMovieFromRemoteUseCaseTests: XCTestCase {
         XCTAssertTrue(client.requestedURLs.isEmpty)
     }
     
-    func test_load_requestDataFRomURL() {
-        let (sut, client) = makeSUT()
+    func test_load_requestsDataFromURL() {
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT(url: url)
         
         sut.load { _ in }
-        XCTAssertNotNil(client.requestedURLs)
+        
+        XCTAssertEqual(client.requestedURLs, [url])
     }
     
-    func test_loadTwice_requestsDataFRomURLTwice() {
+    func test_loadTwice_requestsDataFromURLTwice() {
         let url = URL(string: "https://a-given-url.com")!
         let (sut, client) = makeSUT(url: url)
         
@@ -62,23 +64,24 @@ class LoadMovieFromRemoteUseCaseTests: XCTestCase {
     func test_load_deliversErrorOnClientError() {
         let (sut, client) = makeSUT()
         
-        var capturedError = [RemoteMovieLoader.Error]()
-        sut.load { capturedError.append($0) }
+        var capturedErrors = [RemoteMovieLoader.Error]()
+        sut.load { capturedErrors.append($0) }
         
         let clientError = NSError(domain: "Test", code: 0)
         client.complete(with: clientError)
         
-        XCTAssertEqual(capturedError, [.connectivity])
+        XCTAssertEqual(capturedErrors, [.connectivity])
     }
     
     func test_load_deliversErrorOnNon200HTTPResponse() {
         let (sut, client) = makeSUT()
-
-        let sample = [199, 201, 300, 400, 500]
         
-        sample.enumerated().forEach{ code, index in
+        let samples = [199, 201, 300, 400, 500]
+        
+        samples.enumerated().forEach { index, code in
             var capturedErrors = [RemoteMovieLoader.Error]()
             sut.load { capturedErrors.append($0) }
+            
             client.complete(withStatusCode: code, at: index)
             
             XCTAssertEqual(capturedErrors, [.invalidData])
