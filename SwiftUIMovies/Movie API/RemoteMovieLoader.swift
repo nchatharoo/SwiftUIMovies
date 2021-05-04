f5caf7a5ec8b86e2503ea59601299803abf92a9a
Nadheer Chatharoo <>
2021-05-04 12:02:47 +0200

    Delivers invalid data error on non 200 HTTPResponse

--- SwiftUIMovies/Movie API/RemoteMovieLoader.swift
+++ SwiftUIMovies/Movie API/RemoteMovieLoader.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 public protocol HTTPClient {
-    func get(from url: URL, completion: @escaping (Error) -> Void)
+    func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void)
 }
 
 public final class RemoteMovieLoader {
@@ -16,6 +16,7 @@ public final class RemoteMovieLoader {
     public let client: HTTPClient
     
     public enum Error: Swift.Error {
+        case invalidData
         case connectivity
     }
     
@@ -25,8 +26,12 @@ public final class RemoteMovieLoader {
     }
     
     public func load(completion: @escaping (Error) -> Void) {
-        client.get(from: url) {error in
-            completion(.connectivity)
+        client.get(from: url) { error, response in
+            if response != nil {
+                completion(.invalidData)
+            } else {
+                completion(.connectivity)
+            }
         }
     }
 }
--- SwiftUIMoviesTests/LoadMovieFromRemoteUseCaseTests.swift
+++ SwiftUIMoviesTests/LoadMovieFromRemoteUseCaseTests.swift
@@ -9,18 +9,28 @@ import XCTest
 import SwiftUIMovies
 
 class HTTPClientSpy: HTTPClient {
-    private var messages = [(url: URL, completion: (Error) -> Void)]()
+    private var messages = [(url: URL, completion: (Error?, HTTPURLResponse?) -> Void)]()
     
     var requestedURLs: [URL] {
         return messages.map { $0.url }
     }
 
-    func get(from url: URL, completion: @escaping (Error) -> Void) {
+    func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void) {
         messages.append((url, completion))
     }
     
     func complete(with error: Error, at index: Int = 0) {
-        messages[index].completion(error)
+        messages[index].completion(error, nil)
+    }
+    
+    func complete(withStatusCode code: Int, at index: Int = 0) {
+        let response = HTTPURLResponse(
+            url: requestedURLs[index],
+            statusCode: code,
+            httpVersion: nil,
+            headerFields: nil
+        )
+        messages[index].completion(nil, response)
     }
 }
 
@@ -59,6 +69,17 @@ class LoadMovieFromRemoteUseCaseTests: XCTestCase {
         client.complete(with: clientError)
         
         XCTAssertEqual(capturedError, [.connectivity])
+    }
+    
+    func test_load_deliversErrorOnNon200HTTPResponse() {
+        let (sut, client) = makeSUT()
+
+        var capturedErrors = [RemoteMovieLoader.Error]()
+        sut.load { capturedErrors.append($0) }
+        
+        client.complete(withStatusCode: 400)
+
+        XCTAssertEqual(capturedErrors, [.invalidData])
     }
     
     // MARK: - Helpers
