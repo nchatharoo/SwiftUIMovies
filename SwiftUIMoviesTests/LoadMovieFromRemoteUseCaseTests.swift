b7d4a969b8dfe7488bc45d7eacbf549f172ccc1f
Nadheer Chatharoo <>
2021-05-04 13:41:07 +0200

    Delivers invalid data error on 200 HTTP response with invalid JSON

--- SwiftUIMoviesTests/LoadMovieFromRemoteUseCaseTests.swift
+++ SwiftUIMoviesTests/LoadMovieFromRemoteUseCaseTests.swift
@@ -23,15 +23,16 @@ class HTTPClientSpy: HTTPClient {
         messages[index].completion(.failure(error))
     }
     
-    func complete(withStatusCode code: Int, at index: Int = 0) {
+    func complete(withStatusCode code: Int, data: Data = Data(), at index: Int = 0) {
         let response = HTTPURLResponse(
             url: requestedURLs[index],
             statusCode: code,
             httpVersion: nil,
             headerFields: nil
         )!
-        messages[index].completion(.success(response))
+        messages[index].completion(.success(data, response))
     }
+    
 }
 
 class LoadMovieFromRemoteUseCaseTests: XCTestCase {
@@ -88,6 +89,18 @@ class LoadMovieFromRemoteUseCaseTests: XCTestCase {
         }
     }
     
+    func test_load_deliversErrorOn200HTTPResponseWithInvalidJSON() {
+        let (sut, client) = makeSUT()
+        
+        var capturedErrors = [RemoteMovieLoader.Error]()
+        sut.load { capturedErrors.append($0) }
+        
+        let invalidJSON = Data("invalid json".utf8)
+        client.complete(withStatusCode: 200, data: invalidJSON)
+        
+        XCTAssertEqual(capturedErrors, [.invalidData])
+    }
+
     // MARK: - Helpers
     
     private func makeSUT(url: URL = URL(string: "https://a-url.com")!, file: StaticString = #file, line: UInt = #line) -> (sut: RemoteMovieLoader, client: HTTPClientSpy) {
