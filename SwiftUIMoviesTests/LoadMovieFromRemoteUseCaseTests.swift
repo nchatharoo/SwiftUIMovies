9c8b8c7d4d6e7176142f762de76f8e76333a2af0
Nadheer Chatharoo <>
2021-05-04 11:47:26 +0200

    Combine Spy capture params into a single type (tuple)

--- SwiftUIMoviesTests/LoadMovieFromRemoteUseCaseTests.swift
+++ SwiftUIMoviesTests/LoadMovieFromRemoteUseCaseTests.swift
@@ -9,16 +9,18 @@ import XCTest
 import SwiftUIMovies
 
 class HTTPClientSpy: HTTPClient {
-    var requestedURLs = [URL]()
-    var completions = [(Error) -> Void]()
+    private var messages = [(url: URL, completion: (Error) -> Void)]()
+    
+    var requestedURLs: [URL] {
+        return messages.map { $0.url }
+    }
 
     func get(from url: URL, completion: @escaping (Error) -> Void) {
-        completions.append(completion)
-        requestedURLs.append(url)
+        messages.append((url, completion))
     }
     
     func complete(with error: Error, at index: Int = 0) {
-        completions[index](error)
+        messages[index].completion(error)
     }
 }
 
