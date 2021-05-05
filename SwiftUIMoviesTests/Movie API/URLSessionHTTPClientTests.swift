6ff0f47957c75254297ad08249cc172c8a489891
Nadheer Chatharoo <nchatharoo@icloud.com>
2021-05-05 20:13:06 +0200

    Move the URLSessionHTTPClient creation to a factory method

--- SwiftUIMoviesTests/Movie API/URLSessionHTTPClientTests.swift
+++ SwiftUIMoviesTests/Movie API/URLSessionHTTPClientTests.swift
@@ -36,17 +36,30 @@ class URLSessionHTTPClientTests: XCTestCase {
         URLProtocolStub.stopInterceptingRequests()
     }
     
+    func test_getFromURL_performsGETRequestWithURL() {
+        let url = URL(string: "http://any-url.com")!
+        let exp = expectation(description: "Wait for request")
+        
+        URLProtocolStub.observeRequests { request in
+            XCTAssertEqual(request.url, url)
+            XCTAssertEqual(request.httpMethod, "GET")
+            exp.fulfill()
+        }
+        
+        makeSUT().get(from: url) { _ in }
+        
+        wait(for: [exp], timeout: 1.0)
+    }
+    
     func test_getFromURL_failsOnRequestError() {
         
         let url = URL(string: "http://any-url.com")!
         let error = NSError(domain: "any error", code: 1)
         URLProtocolStub.stub(data: nil, response: nil, error: error)
-        
-        let sut = URLSessionHTTPClient()
-        
+                
         let exp = expectation(description: "Wait for completion")
         
-        sut.get(from: url) { result in
+        makeSUT().get(from: url) { result in
             switch result {
             case let .failure(receivedError as NSError):
                 XCTAssertEqual(receivedError.domain, error.domain)
@@ -130,5 +143,9 @@ class URLSessionHTTPClientTests: XCTestCase {
         }
         
         override func stopLoading() {}
+    }
+    
+    private func makeSUT() -> URLSessionHTTPClient {
+        return URLSessionHTTPClient()
     }
 }
