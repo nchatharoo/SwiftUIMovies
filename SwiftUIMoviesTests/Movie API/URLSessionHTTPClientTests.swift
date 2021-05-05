9bbcfe2df453af2a68f2482bd127dee492994bb0
Nadheer Chatharoo <nchatharoo@icloud.com>
2021-05-05 17:00:11 +0200

    Creates URLSession data task with URL on URLSessionHTTPClient.get(from url)

--- SwiftUIMoviesTests/Movie API/URLSessionHTTPClientTests.swift
+++ SwiftUIMoviesTests/Movie API/URLSessionHTTPClientTests.swift
@@ -7,26 +7,38 @@
 
 import XCTest
 
+class URLSessionHTTPClient {
+    private let session: URLSession
+    
+    init(session: URLSession) {
+        self.session = session
+    }
+    
+    func get(from url: URL) {
+        session.dataTask(with: url) { _, _, _ in }
+    }
+}
+
 class URLSessionHTTPClientTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func test_getFromURL_createsDataTaskWithURL() {
+        let url = URL(string: "http://any-url.com")!
+        let session = URLSessionSpy()
+        
+        let sut = URLSessionHTTPClient(session: session)
+        sut.get(from: url)
+        XCTAssertEqual(session.receivedURLs, [url])
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    // MARK: Helpers
+    private class URLSessionSpy: URLSession {
+        var receivedURLs = [URL]()
+        
+        override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+            receivedURLs.append(url)
+            return FakeURLSessionDataTask()
         }
     }
-
+    
+    private class FakeURLSessionDataTask: URLSessionDataTask {}
 }
