04962f02afc96939f15a79bf7db905bc5046237f
Nadheer Chatharoo <nchatharoo@icloud.com>
2021-05-05 21:26:17 +0200

    Make test get from url succeed again by comparing error domain

--- SwiftUIMoviesTests/Movie API/URLSessionHTTPClientTests.swift
+++ SwiftUIMoviesTests/Movie API/URLSessionHTTPClientTests.swift
@@ -62,9 +62,9 @@ class URLSessionHTTPClientTests: XCTestCase {
     func test_getFromURL_failsOnRequestError() {
         let requestError = anyNSError()
         
-        let receivedError = resultErrorFor(data: nil, response: nil, error: requestError)
+        let receivedError = resultErrorFor(data: nil, response: nil, error: requestError) as NSError?
         
-        XCTAssertEqual(receivedError as NSError?, requestError)
+        XCTAssertEqual(receivedError?.domain, requestError.domain)
     }
     
     func test_getFromURL_failsOnAllInvalidRepresentationCases() {
