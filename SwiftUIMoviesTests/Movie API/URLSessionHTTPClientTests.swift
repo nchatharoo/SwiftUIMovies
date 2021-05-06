77c0857e5e5abc6476ce6c0a660e4143919346f1
Nadheer Chatharoo <nchatharoo@icloud.com>
2021-05-06 20:10:08 +0200

    Add a enum endpoint

--- SwiftUIMoviesTests/Movie API/URLSessionHTTPClientTests.swift
+++ SwiftUIMoviesTests/Movie API/URLSessionHTTPClientTests.swift
@@ -8,6 +8,25 @@
 import XCTest
 import SwiftUIMovies
 
+enum MovieListEndpoint: String, CaseIterable, Identifiable {
+    
+    var id: String { rawValue }
+    
+    case nowPlaying = "now_playing"
+    case upcoming
+    case topRated = "top_rated"
+    case popular
+    
+    var description: String {
+        switch self {
+            case .nowPlaying: return "Now Playing"
+            case .upcoming: return "Upcoming"
+            case .topRated: return "Top Rated"
+            case .popular: return "Popular"
+        }
+    }
+}
+
 class URLSessionHTTPClientTests: XCTestCase {
     private let baseAPIURL = "https://api.themoviedb.org/3"
 
