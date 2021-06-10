d642081b98548dba2113ab3217aa1302b6938be5
Nadheer Chatharoo <nchatharoo@icloud.com>
2021-06-10 15:17:45 +0200

    add end-to-end tests to separate test target

---
+++ SwiftUIMoviesAPIEndToEndTests/SwiftUIMoviesAPIEndToEndTests.swift
@@ -0,0 +1,14 @@
+//
+//  SwiftUIMoviesAPIEndToEndTests.swift
+//  SwiftUIMoviesAPIEndToEndTests
+//
+//  Created by Nadheer on 10/06/2021.
+//
+
+import XCTest
+
+class SwiftUIMoviesAPIEndToEndTests: XCTestCase {
+    func test_init() {
+        
+    }
+}
