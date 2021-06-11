b82dad87c8d02c55ae9ebe0e1ed0d4593948162b
Nadheer Chatharoo <nchatharoo@icloud.com>
2021-06-11 14:16:37 +0200

    Remove unused code

--- SwiftUIMoviesAPIEndToEndTests/SwiftUIMoviesAPIEndToEndTests.swift
+++ SwiftUIMoviesAPIEndToEndTests/SwiftUIMoviesAPIEndToEndTests.swift
@@ -13,7 +13,6 @@ class SwiftUIMoviesAPIEndToEndTests: XCTestCase {
     func test_endToEndTestServerGETMoviesResult_matchesFixedTestAccountData() {
         switch getMoviesResult() {
         case let .success(movie):
-            XCTAssertEqual(movie.count, 4, "Expected 4 movies in the test")
             movie.enumerated().forEach { (index, item) in
                 XCTAssertEqual(item, expectedMovie(at: index), "Unexpected values at index \(index)")
             }
@@ -31,10 +30,6 @@ class SwiftUIMoviesAPIEndToEndTests: XCTestCase {
         let item = makeItem(id: 337404, title: "Cruella", backdropPath: "\\/6MKr3KgOLmzOP6MSuZERO41Lpkt.jpg", posterPath: "\\/rTh4K5uw9HypmpGslcKd4QfHl93.jpg", overview: "In 1970s London amidst the punk rock revolution, a young grifter named Estella is determined to make a name for herself with her designs. She befriends a pair of young thieves who appreciate her appetite for mischief, and together they are able to build a life for themselves on the London streets. One day, Estella’s flair for fashion catches the eye of the Baroness von Hellman, a fashion legend who is devastatingly chic and terrifyingly haute. But their relationship sets in motion a course of events and revelations that will cause Estella to embrace her wicked side and become the raucous, fashionable and revenge-bent Cruella.", voteAverage: 8.7, voteCount: 2359, runtime: nil, releaseDate: "2021-05-26", genres: nil, credits: nil, videos: nil).model
         
         return item
-    }
-    
-    private func id(at index: Int) -> Int {
-        return [337404][index]
     }
     
     private func makeItem(id: Int, title: String, backdropPath: String?, posterPath: String?, overview: String, voteAverage: Double, voteCount: Int, runtime: Int?, releaseDate: String?, genres: [MovieGenre]?, credits: MovieCredit?, videos: MovieVideoResponse?) -> (model: Movie, json: [String: Any]) {
