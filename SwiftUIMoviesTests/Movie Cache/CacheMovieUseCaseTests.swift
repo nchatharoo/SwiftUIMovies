294a97d444f1876f87f0d8a593265f24ea40c336
Nadheer Chatharoo <nchatharoo@icloud.com>
2021-06-15 14:44:57 +0200

    Save command succeeds on successful cache insertion

--- SwiftUIMoviesTests/Movie Cache/CacheMovieUseCaseTests.swift
+++ SwiftUIMoviesTests/Movie Cache/CacheMovieUseCaseTests.swift
@@ -63,6 +63,10 @@ class MovieStore {
     func completeInsertion(with error: Error, at index: Int = 0) {
         insertionCompletions[index](error)
     }
+    
+    func completeInsertionSuccessfully(at index: Int = 0) {
+        insertionCompletions[index](nil)
+    }
 }
 
 class CacheMovieUseCaseTests: XCTestCase {
@@ -138,7 +142,22 @@ class CacheMovieUseCaseTests: XCTestCase {
         XCTAssertEqual(receivedError as NSError?, insertionError)
     }
     
-
+    func test_save_succedsOnSuccessfulCacheInsertion() {
+        let (sut, store) = makeSUT()
+        let items = [uniqueItem(), uniqueItem()]
+        let exp = expectation(description: "Wait for save completion")
+        
+        var receivedError: Error?
+        sut.save(items) { error in
+            receivedError = error
+            exp.fulfill()
+        }
+        store.completeDeletionSuccessfully()
+        store.completeInsertionSuccessfully()
+        wait(for: [exp], timeout: 1.0)
+        
+        XCTAssertNil(receivedError as NSError?)
+    }
     
     //MARK: - Helpers
     
