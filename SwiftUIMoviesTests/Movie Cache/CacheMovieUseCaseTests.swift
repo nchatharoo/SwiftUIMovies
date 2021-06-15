badd670491171260b71901b6c2b76f750de9862d
Nadheer Chatharoo <nchatharoo@icloud.com>
2021-06-15 16:24:21 +0200

    Invert if logic to make code paths easier to follow

--- SwiftUIMoviesTests/Movie Cache/CacheMovieUseCaseTests.swift
+++ SwiftUIMoviesTests/Movie Cache/CacheMovieUseCaseTests.swift
@@ -21,13 +21,14 @@ class LocalMovieLoader {
         store.deleteCacheMovie { [weak self] error in
             guard let self = self else { return }
 
-            if error == nil {
+            if let cacheDeletionError = error {
+                completion(cacheDeletionError)
+            } else {
+
                 self.store.insert(items, timestamp: self.currentDate()) { [weak self] error in
                     guard self != nil else { return }
                     completion(error)
                 }
-            } else {
-                completion(error)
             }
         }
     }
