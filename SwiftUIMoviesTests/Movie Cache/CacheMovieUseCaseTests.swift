e9541c9496d2b8228a32849e50a38b99ac29fbe9
Nadheer Chatharoo <nchatharoo@icloud.com>
2021-06-15 10:16:39 +0200

    Save command requests cache deletion

--- SwiftUIMoviesTests/Movie Cache/CacheMovieUseCaseTests.swift
+++ SwiftUIMoviesTests/Movie Cache/CacheMovieUseCaseTests.swift
@@ -6,23 +6,54 @@
 //
 
 import XCTest
+import SwiftUIMovies
 
 class LocalMovieLoader {
+    private let store: MovieStore
+    
     init(store: MovieStore) {
-        
+        self.store = store
+    }
+    
+    func save(_ items: [Movie]) {
+        store.deleteCacheMovie()
     }
 }
 
 class MovieStore {
     var deleteCacheMovieCount = 0
+    
+    func deleteCacheMovie() {
+        deleteCacheMovieCount += 1
+    }
 }
 
 class CacheMovieUseCaseTests: XCTestCase {
-
+    
     func test_init_doesNotDeleteCacheUponCreation() {
         let store = MovieStore()
         _ = LocalMovieLoader(store: store)
         XCTAssertEqual(store.deleteCacheMovieCount, 0)
     }
-
+    
+    func test_save_requestsCacheDeletion() {
+        let store = MovieStore()
+        let sut = LocalMovieLoader(store: store)
+        let items = [uniqueItem(), uniqueItem()]
+        
+        sut.save(items)
+        
+        XCTAssertEqual(store.deleteCacheMovieCount, 1)
+    }
+    
+    //MARK: - Helpers
+    
+    private func uniqueItem() -> Movie {
+        
+        return Movie(id: anyInt(), title: "Bloodshot", backdropPath: "\\/ocUrMYbdjknu2TwzMHKT9PBBQRw.jpg", posterPath: "\\/8WUVHemHFH2ZIP6NWkwlHWsyrEL.jpg", overview: "", voteAverage: 7.1, voteCount: 418, runtime: nil, releaseDate: "2020-03-05", genres: nil, credits: nil, videos: nil)
+    }
+    
+    private func anyInt() -> Int {
+        return [338762, 336845, 338482].randomElement()!
+    }
 }
