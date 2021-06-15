2338998186e6a91fb129164697c71e6e16bbcab2
Nadheer Chatharoo <nchatharoo@icloud.com>
2021-06-15 16:20:10 +0200

    Guarantee that the LocalMovieLoader does not deliver insertion error after instance has been deallocated

--- SwiftUIMoviesTests/Movie Cache/CacheMovieUseCaseTests.swift
+++ SwiftUIMoviesTests/Movie Cache/CacheMovieUseCaseTests.swift
@@ -22,7 +22,10 @@ class LocalMovieLoader {
             guard let self = self else { return }
 
             if error == nil {
-                self.store.insert(items, timestamp: self.currentDate(), completion: completion)
+                self.store.insert(items, timestamp: self.currentDate()) { [weak self] error in
+                    guard self != nil else { return }
+                    completion(error)
+                }
             } else {
                 completion(error)
             }
