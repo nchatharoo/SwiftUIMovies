d4305190dcbe7efce1141a1db0bb1641daf45469
Nadheer Chatharoo <nchatharoo@icloud.com>
2021-06-15 14:33:42 +0200

    Save command fails on cache deletion error

--- SwiftUIMoviesTests/Movie Cache/CacheMovieUseCaseTests.swift
+++ SwiftUIMoviesTests/Movie Cache/CacheMovieUseCaseTests.swift
@@ -17,8 +17,9 @@ class LocalMovieLoader {
         self.currentDate = currentDate
     }
     
-    func save(_ items: [Movie]) {
+    func save(_ items: [Movie], completion: @escaping (Error?) -> Void) {
         store.deleteCacheMovie { [unowned self] error in
+            completion(error)
             if error == nil {
                 store.insert(items, timestamp: self.currentDate())
             }
@@ -67,7 +68,7 @@ class CacheMovieUseCaseTests: XCTestCase {
         let (sut, store) = makeSUT()
         let items = [uniqueItem(), uniqueItem()]
         
-        sut.save(items)
+        sut.save(items) { _ in }
         
         XCTAssertEqual(store.receivedMessages, [.deleteCacheMovie])
     }
@@ -77,7 +78,7 @@ class CacheMovieUseCaseTests: XCTestCase {
         let items = [uniqueItem(), uniqueItem()]
         let deletionError = anyNSError()
         
-        sut.save(items)
+        sut.save(items) { _ in }
         store.completeDeletion(with: deletionError)
         
         XCTAssertEqual(store.receivedMessages, [.deleteCacheMovie])
@@ -88,11 +89,29 @@ class CacheMovieUseCaseTests: XCTestCase {
         let (sut, store) = makeSUT(currentDate: { timestamp })
         let items = [uniqueItem(), uniqueItem()]
         
-        sut.save(items)
+        sut.save(items) { _ in }
         store.completeDeletionSuccessfully()
         
         XCTAssertEqual(store.receivedMessages, [.deleteCacheMovie, .insert(items, timestamp)])
     }
+    
+    func test_save_failsOnDeletionError() {
+        let (sut, store) = makeSUT()
+        let items = [uniqueItem(), uniqueItem()]
+        let deletionError = anyNSError()
+        let exp = expectation(description: "Wait for save completion")
+        
+        var receivedError: Error?
+        sut.save(items) { error in
+            receivedError = error
+            exp.fulfill()
+        }
+        store.completeDeletion(with: deletionError)
+        wait(for: [exp], timeout: 1.0)
+        
+        XCTAssertEqual(receivedError as NSError?, deletionError)
+    }
+
     
     
 
