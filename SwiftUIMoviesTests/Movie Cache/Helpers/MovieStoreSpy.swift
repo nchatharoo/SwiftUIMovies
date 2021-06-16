750a4c6aac622b105d9b95d572435c416293eebe
Nadheer Chatharoo <nchatharoo@icloud.com>
2021-06-16 13:07:51 +0200

    Extract MovieStoreSpy into a shared scope to remove duplication

---
+++ SwiftUIMoviesTests/Movie Cache/Helpers/MovieStoreSpy.swift
@@ -0,0 +1,47 @@
+//
+//  MovieStoreSpy.swift
+//  SwiftUIMoviesTests
+//
+//  Created by Nadheer on 16/06/2021.
+//
+
+import Foundation
+import SwiftUIMovies
+
+class MovieStoreSpy: MovieStore {
+    enum ReceivedMessage: Equatable {
+        case deleteCacheMovie
+        case insert([LocalMovieItem],Date)
+    }
+    
+    private(set) var receivedMessages = [ReceivedMessage]()
+    
+    private var deletionCompletions = [DeletionCompletion]()
+    private var insertionCompletions = [InsertionCompletion]()
+    
+    func deleteCacheMovie(completion: @escaping DeletionCompletion) {
+        deletionCompletions.append(completion)
+        receivedMessages.append(.deleteCacheMovie)
+    }
+
+    func completeDeletion(with error: Error, at index: Int = 0) {
+        deletionCompletions[index](error)
+    }
+    
+    func completeDeletionSuccessfully(at index: Int = 0) {
+        deletionCompletions[index](nil)
+    }
+    
+    func insert(_ items: [LocalMovieItem], timestamp: Date, completion: @escaping InsertionCompletion) {
+        insertionCompletions.append(completion)
+        receivedMessages.append(.insert(items, timestamp))
+    }
+    
+    func completeInsertion(with error: Error, at index: Int = 0) {
+        insertionCompletions[index](error)
+    }
+    
+    func completeInsertionSuccessfully(at index: Int = 0) {
+        insertionCompletions[index](nil)
+    }
+}
