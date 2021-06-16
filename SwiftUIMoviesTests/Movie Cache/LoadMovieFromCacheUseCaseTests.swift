a9195e4803d5054508712dc0ebeaaeea2c9160b2
Nadheer Chatharoo <nchatharoo@icloud.com>
2021-06-16 16:32:15 +0200

    Load command does not delete cache when cache is already empty

--- SwiftUIMoviesTests/Movie Cache/LoadMovieFromCacheUseCaseTests.swift
+++ SwiftUIMoviesTests/Movie Cache/LoadMovieFromCacheUseCaseTests.swift
@@ -81,6 +81,15 @@ class LoadMovieFromCacheUseCaseTests: XCTestCase {
         XCTAssertEqual(store.receivedMessages, [.retrieve, .deleteCacheMovie])
     }
     
+    func test_load_doesNotDeletesCacheOnEmptyCache() {
+        let (sut, store) = makeSUT()
+        
+        sut.load { _ in }
+        store.completeRetrievalWithEmptyCache()
+                
+        XCTAssertEqual(store.receivedMessages, [.retrieve])
+    }
+    
     //MARK: - Helpers
     
     private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalMovieLoader, store: MovieStoreSpy) {
