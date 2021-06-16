6e5c800fae6b27ec74240fcdc103c2aa37bb83bb
Nadheer Chatharoo <nchatharoo@icloud.com>
2021-06-16 12:16:26 +0200

    Move RemoteMovieItem to its own file

--- SwiftUIMovies/Movie API/RemoteMovieLoader.swift
+++ SwiftUIMovies/Movie API/RemoteMovieLoader.swift
@@ -66,22 +66,6 @@ private extension Array where Element == RemoteMovieItem {
     }
 }
 
-internal struct RemoteMovieItem: Decodable {
-    internal let id: Int
-    internal let title: String
-    internal let backdropPath: String?
-    internal let posterPath: String?
-    internal let overview: String
-    internal let voteAverage: Double
-    internal let voteCount: Int
-    internal let runtime: Int?
-    internal let releaseDate: String?
-    
-    internal let genres: [MovieGenre]?
-    internal let credits: MovieCredit?
-    internal let videos: MovieVideoResponse?
-}
-
 final class MovieItemsMapper {
     private struct Root: Decodable {
         let results: [RemoteMovieItem]
