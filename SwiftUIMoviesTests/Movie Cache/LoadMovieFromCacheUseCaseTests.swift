a6ece395aefe747cc10c1224d2d348c1e455d07e
Nadheer Chatharoo <nchatharoo@icloud.com>
2021-06-17 12:02:10 +0200

    Extract duplicate test helpers into a shared scope

--- SwiftUIMoviesTests/Movie Cache/LoadMovieFromCacheUseCaseTests.swift
+++ SwiftUIMoviesTests/Movie Cache/LoadMovieFromCacheUseCaseTests.swift
@@ -169,34 +169,4 @@ class LoadMovieFromCacheUseCaseTests: XCTestCase {
         action()
         wait(for: [exp], timeout: 1.0)
     }
-
-    private func uniqueItem() -> Movie {
-        
-        return Movie(id: anyInt(), title: "Bloodshot", backdropPath: "\\/ocUrMYbdjknu2TwzMHKT9PBBQRw.jpg", posterPath: "\\/8WUVHemHFH2ZIP6NWkwlHWsyrEL.jpg", overview: "", voteAverage: 7.1, voteCount: 418, runtime: nil, releaseDate: "2020-03-05", genres: nil, credits: nil, videos: nil)
-    }
-    
-    private func anyInt() -> Int {
-        return [338762, 336845, 338482].randomElement()!
-    }
-    
-    private func anyNSError() -> NSError {
-        return NSError(domain: "any error", code: 0, userInfo: nil)
-    }
-    
-    private func uniqueItems() -> (models: [Movie], local: [LocalMovieItem]) {
-        let models = [uniqueItem(), uniqueItem()]
-        let local = models.map { LocalMovieItem(id: $0.id, title: $0.title, backdropPath: $0.backdropPath, posterPath: $0.posterPath, overview: $0.overview, voteAverage: $0.voteAverage, voteCount: $0.voteCount, runtime: $0.runtime, releaseDate: $0.releaseDate, genres: $0.genres, credits: $0.credits, videos: $0.videos) }
-        return (models, local)
-    }
-
-}
-
-private extension Date {
-    func adding(days: Int) -> Date {
-        return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
-    }
-    
-    func adding(seconds: TimeInterval) -> Date {
-        return self + seconds
-    }
 }
