39b89351959e3adee17fc2ffc45268e0923a67ed
Nadheer Chatharoo <nchatharoo@icloud.com>
2021-06-17 09:38:13 +0200

    LocalMovieLoader does not message store upon creation (before validating the cache)

---
+++ SwiftUIMoviesTests/Movie Cache/ValidateFeedCacheUseCaseTests.swift
@@ -0,0 +1,33 @@
+//
+//  ValidateFeedCacheUseCaseTests.swift
+//  SwiftUIMoviesTests
+//
+//  Created by Nadheer on 17/06/2021.
+//
+
+import XCTest
+import SwiftUIMovies
+
+class ValidateFeedCacheUseCaseTests: XCTestCase {
+    
+    func test_init_doesNotMessageStoreUponCreation() {
+        let (_, store) = makeSUT()
+        
+        XCTAssertEqual(store.receivedMessages, [])
+    }
+    
+    //MARK: - Helpers
+    
+    private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalMovieLoader, store: MovieStoreSpy) {
+        let store = MovieStoreSpy()
+        let sut = LocalMovieLoader(store: store, currentDate: currentDate)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+
+        return (sut, store)
+    }
+    
+    private func anyNSError() -> NSError {
+        return NSError(domain: "any error", code: 0, userInfo: nil)
+    }
+}
