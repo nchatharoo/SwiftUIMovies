e6600bdbd0984054d2aca501fb2a18764c367484
Nadheer Chatharoo <nchatharoo@icloud.com>
2021-08-09 16:43:08 +0200

    Add SwiftUIMoviesCacheIntegrationTests target to separate the potentially-slower cache integration tests from the fast unit/isolated tests

---
+++ SwiftUIMoviesCacheIntegrationTests/SwiftUIMoviesCacheIntegrationTests.swift
@@ -0,0 +1,13 @@
+//
+//  SwiftUIMoviesCacheIntegrationTests.swift
+//  SwiftUIMoviesCacheIntegrationTests
+//
+//  Created by Nadheer on 09/08/2021.
+//
+
+import XCTest
+
+class SwiftUIMoviesCacheIntegrationTests: XCTestCase {
+
+
+}
