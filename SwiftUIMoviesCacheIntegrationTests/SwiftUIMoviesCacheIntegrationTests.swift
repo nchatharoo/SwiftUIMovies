67a4d707916c324a6b5c9969120bba62ad3e0f90
Nadheer Chatharoo <nchatharoo@icloud.com>
2021-08-09 17:05:03 +0200

    LocalMovieLoader in integration with the CodableMovieStore delivers movies saved on separate instances proving we correctly persist the data models to disk

--- SwiftUIMoviesCacheIntegrationTests/SwiftUIMoviesCacheIntegrationTests.swift
+++ SwiftUIMoviesCacheIntegrationTests/SwiftUIMoviesCacheIntegrationTests.swift
@@ -42,6 +42,34 @@ class SwiftUIMoviesCacheIntegrationTests: XCTestCase {
         wait(for: [exp], timeout: 1.0)
     }
     
+    func test_load_deliversItemsSavedOnSeparateInstance() {
+        let sutToPerfomSave = makeSUT()
+        let sutToPerformLoad = makeSUT()
+        let movies = uniqueItems().models
+        
+        let saveExp = expectation(description: "Wait for save completion")
+        sutToPerfomSave.save(movies) { saveError in
+            XCTAssertNil(saveError, "Expected to save successfully")
+            saveExp.fulfill()
+        }
+        wait(for: [saveExp], timeout: 1.0)
+
+        
+        let loadExp = expectation(description: "Wait for load completion")
+        sutToPerformLoad.load { loadResult in
+            switch loadResult {
+            case let .success(imageFeed):
+                XCTAssertEqual(imageFeed, movies)
+            case let .failure(error):
+                XCTFail("Expected successful feed result, got \(error) instead")
+            }
+            
+            loadExp.fulfill()
+        }
+        
+        wait(for: [loadExp], timeout: 1.0)
+    }
+    
     // - MARK: Helpers
     
     private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> LocalMovieLoader {
