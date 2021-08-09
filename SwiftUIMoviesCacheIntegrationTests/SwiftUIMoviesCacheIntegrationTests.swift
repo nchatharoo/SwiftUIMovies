fd7ee9b5b36fea8e1d68f6b8431e86bf0a67d4b8
Nadheer Chatharoo <nchatharoo@icloud.com>
2021-08-09 16:54:57 +0200

    LocalMovieLoader in integration with the CodableMovieStore delivers no items on empty cache

--- SwiftUIMoviesCacheIntegrationTests/SwiftUIMoviesCacheIntegrationTests.swift
+++ SwiftUIMoviesCacheIntegrationTests/SwiftUIMoviesCacheIntegrationTests.swift
@@ -6,8 +6,48 @@
 //
 
 import XCTest
+import SwiftUIMovies
 
 class SwiftUIMoviesCacheIntegrationTests: XCTestCase {
+
+    func test_load_deliversNoItemsOnEmptyCache() {
+        let sut = makeSUT()
+        
+        let exp = expectation(description: "Wait for load completion")
+        
+        sut.load { result in
+            switch result {
+            
+            case let .success(movies):
+                XCTAssertEqual(movies, [], "Expected empty movies result")
+                
+            case let .failure(error):
+                XCTFail("Expected successful movies result, got \(error) instead")
+            }
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    // - MARK: Helpers
+    
+    private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> LocalMovieLoader {
+        let store = CodableMovieStore(storeURL: testSpecificStoreURL())
+        let sut = LocalMovieLoader(store: store, currentDate: currentDate)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+
+        return sut
+    }
+
+    private func testSpecificStoreURL() -> URL {
+        return FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("\(type(of: self)).store")
+    }
+    
+    private func cachesDirectory() -> URL {
+        return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
+    }
 
 
 }
