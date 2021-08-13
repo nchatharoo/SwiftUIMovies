cc0d6f7bd4a71d4c98b280f6a1fe6253844a70c1
Nadheer Chatharoo <nchatharoo@icloud.com>
2021-08-13 13:32:09 +0200

    Remove  redundant code from MovieListView

--- SwiftUIMovies/Movie View/MovieListView.swift
+++ SwiftUIMovies/Movie View/MovieListView.swift
@@ -53,97 +53,10 @@ struct MovieListView: View {
             .navigationBarTitle("The MovieDb")
         }
         .onAppear {
-            nowPlaying.loadMovies(with: .nowPlaying)
-            upcoming.loadMovies(with: .upcoming)
-            topRated.loadMovies(with: .topRated)
-            popular.loadMovies(with: .popular)
-        }
-    }
-}
-
-struct MoviePosterScrollView: View {
-    
-    let title: String
-    let movies: [Movie]
-    
-    var body: some View {
-        
-        VStack(alignment: .leading, spacing: 16) {
-            
-            Text(title)
-                .font(.title)
-                .fontWeight(.bold)
-                .padding(.horizontal)
-            
-            ScrollView(.horizontal, showsIndicators: false) {
-                HStack {
-                    ForEach(movies, id: \.self.id) { movie in
-                        MoviePosterView(movie: movie)
-                    }
-                }
-            }
-        }
-    }
-}
-
-struct MoviePosterView: View {
-    
-    let movie: Movie
-    @ObservedObject var imageLoader = ImageLoader()
-
-    var body: some View {
-        ZStack {
-            if self.imageLoader.image != nil {
-                Image(uiImage: self.imageLoader.image!)
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .cornerRadius(8)
-            } else {
-                Rectangle()
-                    .fill(Color.gray.opacity(0.3))
-                    .cornerRadius(8)
-                
-                Text(movie.title)
-                    .multilineTextAlignment(.center)
-            }
-        }
-        .frame(width: 204, height: 306)
-        .onAppear {
-            self.imageLoader.loadImage(with: self.movie.posterURL)
-        }
-    }
-}
-
-private let _imageCache = NSCache<AnyObject, AnyObject>()
-
-class ImageLoader: ObservableObject {
-    
-    @Published var image: UIImage?
-    @Published var isLoading = false
-    
-    var imageCache = _imageCache
-
-    func loadImage(with url: URL) {
-        let urlString = url.absoluteString
-        if let imageFromCache = imageCache.object(forKey: urlString as AnyObject) as? UIImage {
-            self.image = imageFromCache
-            return
-        }
-        
-        DispatchQueue.global(qos: .background).async { [weak self] in
-            guard let self = self else { return }
-            do {
-                let data = try Data(contentsOf: url)
-                guard let image = UIImage(data: data) else {
-                    return
-                }
-                self.imageCache.setObject(image, forKey: urlString as AnyObject)
-                DispatchQueue.main.async { [weak self] in
-                    self?.image = image
-                }
-            } catch {
-                print(error.localizedDescription)
-            }
+            nowPlaying.loadMovies(from: .nowPlaying)
+            upcoming.loadMovies(from: .upcoming)
+            topRated.loadMovies(from: .topRated)
+            popular.loadMovies(from: .popular)
         }
     }
 }
